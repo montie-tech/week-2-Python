109a544fc7a00601cf6adfa120dabece7ef51bd3
montie-tech <montiecarmon2002@gmail.com>
2025-08-10 11:08:38 +0300

    first commit

---
+++ week.py
@@ -0,0 +1,18 @@
+my_list = []  # create empty list
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print(my_list)  # [10, 20, 30, 40]
+my_list.insert(1, 15)
+print(my_list)
+my_list.extend([50, 60, 70])
+print(my_list)
+my_list.pop()
+print(my_list)
+my_list.sort()
+print(my_list)
+index_of_30 = my_list.index(30)
+print(index_of_30)
